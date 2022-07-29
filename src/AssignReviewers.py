644c74153090bb71a3590978b048a9b41f07d15b
Michael D Kinney <michael.d.kinney@intel.com>
2022-07-28 22:14:02 -0700

    Remove input
    Signed-off-by: Michael D Kinney <michael.d.kinney@intel.com>

--- src/AssignReviewers.py
+++ src/AssignReviewers.py
@@ -11,17 +11,9 @@ from   codeowners import CodeOwners
 from   git        import Git
 from   github     import Github
 
-GitHubToken   = os.environ.get('INPUT_GITHUB_TOKEN')
 GitHubPayload = json.loads(os.environ.get('GITHUB_CONTEXT'))
-
-Hub = Github (GitHubToken)
-print (Hub)
-
 Hub = Github (GitHubPayload['token'])
 print (Hub)
-
 print (GitHubPayload['event']['action'])
-
-print (GitHubPayload)
-
-print (GitHubPayload['event']['action'])
+print (os.environ.get('GITHUB_RUN_ID'))
+print (os.environ.get('GITHUB_RUN_NUMBER'))
